eb451ae71cbc413ee5065da3b6cfa6752d13e430
Ilya Slyusarchuk <ilyaniesvb@gmail.com>
2022-03-09 17:20:26 +0100

    Added scaffolding Vue

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <p>Esto es un parrafo que se encuentra dentro de un componente de Vue.js</p>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
