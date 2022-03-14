f432c91777d2c9e04ba4e0fcfeb5ee24eaebbcf1
Ilya Slyusarchuk <ilyaniesvb@gmail.com>
2022-03-14 16:14:42 +0100

    Naming changes and routes

--- resources/js/components/RegisterComponent.vue
+++ resources/js/components/RegisterComponent.vue
@@ -3,7 +3,7 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">EJEMPLO -> formulario de registro con Vue y acceso a BBDD</div>
+                    <div class="card-header">Formulario de registro</div>
 
                     <div class="card-body">
                         <form action="" v-on:submit.prevent="newUser()">
