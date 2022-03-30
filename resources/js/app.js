3b136f140d693c3ff640d619f1d254297eef4975
Ilya Slyusarchuk <ilyaniesvb@gmail.com>
2022-03-30 17:48:43 +0200

    Added some backend for 'Foro'

--- resources/js/app.js
+++ resources/js/app.js
@@ -51,6 +51,7 @@ Vue.component('footer-web', require('./components/Layout/Footer.vue').default);
 Vue.component('landing', require('./components/landingPage.vue').default);
 Vue.component('InfoCards', require('./components/InfoCards.vue').default);
 Vue.component('register-component', require('./components/RegisterComponent.vue').default);
+Vue.component('foro', require('./components/Foro.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
