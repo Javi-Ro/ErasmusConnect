36c6542c86a12ea54b65073599bc3716fbc2b157
Raul <rbm61@alu.ua.es>
2022-03-30 15:35:12 +0200

    Perfil casi terminado, Buefy me da ansiedad y ahora el footer no aparece en el perfil

--- resources/js/components/Profile.vue
+++ resources/js/components/Profile.vue
@@ -9,8 +9,18 @@
                     style="border-radius: 100px; max-width: 200px"
                 >
             </div>
-            <div class="nickname">
-                @{{nickname}}
+            <div class="nickname column-item">
+                <p style="
+                    font-weight: bold;
+                    font-size: 20px;
+                ">
+                {{this.name}}
+                </p>
+                <p style="
+                    opacity: 0.5;
+                ">
+                    @{{this.nickname}}
+                </p>
             </div>
             <!-- Aparecen todas las opciones del perfil -->
             <div class="opciones">
@@ -30,58 +40,86 @@
             </div>
         </div>
         <!-- Columna en la que aparece el nombre, la biografia y debajo las publicaciones  -->
-        <div class="columna" id="der">
-            <div class="background-img">
-
+        <div class="right-column">
+            <div class="columna" id="der">
+                <div class="background-img">
+                </div>
+                <!-- biografía, pais, amigos-->
+                <div class="user-info">
+                    <div class="biografia">
+                        <p>
+                            Curabitur nibh leo, venenatis at sodales ac, volutpat eget lectus. 
+                            Donec ut est vel lorem sodales ultricies. Nullam a metus a odio rutrum posuere at a magna.
+                            Fusce neque nisl, vestibulum sed est vel, porta euismod dolor. Quisque egestas tristique leo pharetra bibendum. Praesent sit amet lacus risus. 
+                            Duis non nisl a ligula tincidunt tincidunt. Morbi sed lorem dolor. Nullam dignissim tempus odio et egestas. 
+                            Nunc nulla odio, congue a lorem non, eleifend accumsan lectus. Pellentesque habitant morbi tristique senectus et 
+                        </p>
+                    </div>
+                    <div class="amigos-ciudad">
+                        <div class="amigos">
+                            <b-button type="is-info is-light">Ver amigos</b-button>
+                        </div>
+                        <div class="ciudad">
+                            <template>
+                                <section>
+                                    <b-field>
+                                        <b-tag rounded size="is-medium"><strong>{{ this.city }}</strong>, {{ this.country }}</b-tag>
+                                    </b-field>
+                                </section>
+                            </template>
+                            <!-- <strong>{{ this.city }}</strong>, {{ this.country }} -->
+                        </div>
+                    </div>
+                </div>
+                <!-- Permite cambiar entre los distintos tipos de publicaciones -->
+                <section>
+                    <b-tabs v-model="activeTab">
+                        <!-- Ver todas las publicaciones del usuario (incluido comentarios) -->
+                        <b-tab-item label="Mis publicaciones">
+                            Lorem ipsum dolor sit amet.
+                        </b-tab-item>
+                        <!-- Ver publicaciones a las que le has dado me gusta -->
+                        <b-tab-item label="Me gusta">
+                            Lorem <br>
+                            ipsum <br>
+                            dolor <br>
+                            sit <br>
+                            amet.
+                        </b-tab-item>
+                        <!-- Ver publicaciones guardadas -->
+                        <b-tab-item label="Guardados">
+                            Nunc nec velit nec libero vestibulum eleifend.
+                            Curabitur pulvinar congue luctus.
+                            Nullam hendrerit iaculis augue vitae ornare.
+                            Maecenas vehicula pulvinar tellus, id sodales felis lobortis eget.
+                        </b-tab-item>
+                    </b-tabs>
+                </section>
             </div>
-            <!-- Donde aparece el nick del usuario y su biografía -->
-            <div class="user-info">
-
-            </div>
-            <!-- Permite cambiar entre los distintos tipos de publicaciones -->
-            <section>
-                <b-tabs v-model="activeTab">
-                    <!-- Ver todas las publicaciones del usuario (incluido comentarios) -->
-                    <b-tab-item label="Mis publicaciones">
-                        Lorem ipsum dolor sit amet.
-                    </b-tab-item>
-                    <!-- Ver publicaciones a las que le has dado me gusta -->
-                    <b-tab-item label="Me gusta">
-                        Lorem <br>
-                        ipsum <br>
-                        dolor <br>
-                        sit <br>
-                        amet.
-                    </b-tab-item>
-                    <!-- Ver publicaciones guardadas -->
-                    <b-tab-item label="Guardados">
-                        Nunc nec velit nec libero vestibulum eleifend.
-                        Curabitur pulvinar congue luctus.
-                        Nullam hendrerit iaculis augue vitae ornare.
-                        Maecenas vehicula pulvinar tellus, id sodales felis lobortis eget.
-                    </b-tab-item>
-                </b-tabs>
-            </section>
         </div>
+
     </div>
 </template>
 <script>
 export default {
     data() {
         return {
-            nickname: null,
+            name: "Pascual Holder",
+            nickname: "NickName",
+            city: "Madrid",
+            country: "ESPAÑA",
             activeTab: 0
         }
-    },
-    methods: {
-        getNickName(nickname) {
-            console.log("Perfil de: " + nickname);
-            this.nickname = nickname;
-        }
-    },
-    mounted() {
-        this.getNickName(this.$route);
     }
+    // methods: {
+    //     getNickName(nickname) {
+    //         console.log("Perfil de: " + nickname);
+    //         this.nickname = nickname;
+    //     }
+    // },
+    // mounted() {
+    //     this.getNickName(this.$route);
+    // }
 }
 </script>
 <style lang="scss" scoped>
@@ -89,11 +127,12 @@ $blue: #00309a;
 $margen-column: 10%;
 // cc es column-content
 $padding-cc: 2%;
+$izq-column-width: 258px;
 #perfil {
     display: flex;
-    margin: 130px 0 0 0;
+    margin: 92px 0 0 0;
     width: 100%;
-    justify-content: center;
+    // justify-content: center;
     flex-direction: row;
 }
 .columna {
@@ -105,13 +144,28 @@ $padding-cc: 2%;
     box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
 }
 #izq {
-    width: 20%;
-    margin: 0 2% 0 $margen-column/2;
+    padding-left: 30px;
+    border-radius: 0px;
+    // margin: 0 2% 0 $margen-column/2;
     // border-right: 0px;
+    border-left: 0px;
+    border-top: 0px;
+    border-bottom: 0px;
+    position: fixed !important;
+    min-width: $izq-column-width;
+    
+}
+// Padre de #der
+.right-column {
+    display:flex;
+    width:100%;
+    justify-content:center;
+    margin: 38px 0 0 $izq-column-width;
+    overflow-y: auto;
+    // max-height: 542px;
 }
 #der {
-    width: 80%;
-    margin: 0 $margen-column 0 0;
+    width:90%;
 }
 // Opciones de la columna de la izquierda
 .opciones {
@@ -130,6 +184,42 @@ $padding-cc: 2%;
     flex-shrink: 0;
     padding: 0.5rem 0.75rem 0.75rem 0.75rem;
     color: $blue;
+}
+.nickname {
+    color: #000;
+    // font-weight: bold;
+    flex-direction: column;
+    justify-content: center;
+}
+
+// CSS relacionado con la columna derecha
+
+.background-img {
+    min-height: 200px;
+    border-radius: 3px;
+    display: flex;
+    background-image: url('/images/bg-profile-default.jpg'); 
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+
+.user-info {
+    display: flex;
+    margin: 10px 0 10px 0;
+    flex-direction: column;
+}
+.amigos-ciudad {
+    margin: 10px 30px 0 30px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+.amigos {
+    display:flex;
+}
+
+.ciudad {
+    display:flex;
 }
 
 // Mucho texto para el botón de editar perfil con animación: https://codepen.io/FluidOfInsanity/pen/RpgvGW
