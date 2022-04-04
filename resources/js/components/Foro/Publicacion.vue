091c00714a255a3bbf570aab320d4fd736f43fb0
Javier Rodríguez <jrj10@alu.ua.es>
2022-04-04 17:07:47 +0200

    Interfaz web de publicacion individual acabada

--- resources/js/components/Foro/Publicacion.vue
+++ resources/js/components/Foro/Publicacion.vue
@@ -1,20 +1,24 @@
 <template>
   <section class="post-container">
-    <filter-bar></filter-bar>
-    <filter-bar-horizontal class="horizontal-menu"></filter-bar-horizontal>
-    <div class="title-bar">
-      <div class="title-bar-img">
-        <img src="images/arrow-left.svg" alt="Arrow left" width="14px" height="11px" @click="goBack();">
+    <div class="post-container-positioned">
+      <filter-bar></filter-bar>
+      <filter-bar-horizontal class="horizontal-menu"></filter-bar-horizontal>
+      <div class="title-bar">
+        <div class="title-bar-img">
+          <img src="images/arrow-left.svg" alt="Arrow left" width="14px" height="11px" @click="goBack();">
+        </div>
+        <p>Publicación</p>
       </div>
-      <p>Publicación</p>
+      <vista-previa-publicacion class="post" :post="post"></vista-previa-publicacion>
+      <div class="comments-container">
+        <div class="post-comment">
+          <b-input class="post-comment-input" placeholder="Comenta..." rounded></b-input>
+          <p class="post-comment-send" @click="sendComment()">Publicar</p>
+        </div>
+        <comentario></comentario>
+        <comentario></comentario>
+      </div>
     </div>
-    <vista-previa-publicacion class="post" :post="post"></vista-previa-publicacion>
-    <div class="post-comment">
-      <b-input class="post-comment-input" placeholder="Comenta..." rounded></b-input>
-      <p class="post-comment-send" @click="sendComment()">Publicar</p>
-    </div>
-    <comentario></comentario>
-    <comentario></comentario> 
   </section>
 </template>
 
@@ -24,7 +28,7 @@
 
     data() {
       return {
-          post: { id: 1, title: "Aquí en Eslovaquia", text: "Hola buienas ocmo estas y tu bien y yo de locos nene.", img_url: 'ejemplo-praga.jpeg', user_id:1, likes:15 , created_at: '2022-04-03 17:47:11'},
+          post: { id: 1, title: "Aquí en Eslovaquia", text: "Hola buienas ocmo estas y tu bien y yo de locos nene.", img_url: 'ejemplo-praga.jpeg', user_id:1, likes:15 , created_at: '2022-04-03 17:47:11'}
       }
     },
 
